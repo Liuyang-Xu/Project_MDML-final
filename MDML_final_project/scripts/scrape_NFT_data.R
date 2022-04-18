d2f20eaff2cc273e5b1f1f40e0c8f4b8ca1ca84b
Liuyang Xu <99773048+Liuyang-Xu@users.noreply.github.com>
2022-04-18 19:23:03 -0400

    Add files via upload

--- MDML_final_project/scripts/scrape_NFT_data.R
+++ MDML_final_project/scripts/scrape_NFT_data.R
@@ -1,19 +1,12 @@
-######################################################################################
-# Part B
-######################################################################################
-
-######################################################################################
+##################################################################################
 # load library
 library(tidyverse)
 library(lubridate)
 library(rvest)
-# library(readr)
 ######################################################################################
 
 ######################################################################################
-### 2.
-
-# download website data
+# website main page
 main_page_url <- 'https://www.larvalabs.com/cryptopunks'
 main_page_response <- read_html(main_page_url)
 
@@ -37,21 +30,44 @@ Punk_name <- unique(parse_number(html_attr(x = sales_page.each, 'title')))
 sales_page_url.each <- unique(paste0('https://www.larvalabs.com', sales_page_url.each))
 
 
-sales_page_url.each_response <- rep(NA, length(sales_page_url.each))
+# sales_page_url.each_response <- vector(mode = "list", length = 1)
+
 # for (i in 1:length(sales_page_url.each)){
 for (i in 1){
-  sales_page_url.each_response[i] <- read_html(sales_page_url.each[i])
+  sales_page_url.each_response <- append(sales_page_url.each_response, read_html(sales_page_url.each[i]))
   
-  
-  
-  # extract sales page information
-  sale <- html_elements(x = sales_page_url.each_response, 
-                        xpath = '//a[@href = "/cryptopunks/sales"]')
-  sales_page_url <- html_attr(x = sales_page, 'href')
-  sales_count <- parse_number(html_text(sales_page)) 
-  
+  # wait for 3 second to avoid HTTP error 429
+  date_time<-Sys.time()
+  while((as.numeric(Sys.time()) - as.numeric(date_time))<3){} #dummy while loop
+  print(i)
 }
 
+# save(sales_page_url.each_response, file = "sales_page_url_each_response.RData")
+# save(main_page_response, file = "test.RData")
 
+# sales_page_url.each_response  %>% toString %>% saveRDS(., "sales_page_url_each_response.RDS")
+# newobject <- readRDS("someobject.RDS") %>% read_html
 
+# saveRDS(as.character(sales_page_url.each_response), "sales_page_url_each_response0.RDS")
 
+# saveRDS(sales_page_url.each_response, "sales_page_url_each_response1.RDS")
+
+i <- 1
+sales_page_url.each_response <- read_html(sales_page_url.each[i])
+
+# extract sales page information
+punk_gender <- html_elements(x = sales_page_url.each_response, 
+                              xpath = '//div[@id="punkDetails"]//h4') %>% 
+  html_text()
+
+punk_attributes <- html_elements(x = sales_page_url.each_response, 
+                              xpath = '//div[@id="punkDetails"]//div[@class="row detail-row"]//a') %>% 
+  html_text()
+punk_attributes <- punk_attributes[2: (2+parse_number(punk_attributes[1])-1)]
+
+sale_history <- html_elements(x = sales_page_url.each_response, 
+                      xpath = '//div[@id="punkHistory"]//td') %>% 
+  html_text()
+sale_history <- as_tibble(matrix(sale_history, 
+                                 nrow = length(sale_history)/5, ncol = 5, 
+                                 byrow=TRUE))
