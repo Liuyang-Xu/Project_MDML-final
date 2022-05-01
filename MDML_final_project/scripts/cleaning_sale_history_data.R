a7c76e7ac58502247a8596624d443398538b2224
Liuyang Xu <99773048+Liuyang-Xu@users.noreply.github.com>
2022-05-01 09:51:09 -0400

    Add files via upload

--- MDML_final_project/scripts/cleaning_sale_history_data.R
+++ MDML_final_project/scripts/cleaning_sale_history_data.R
@@ -10,30 +10,26 @@ library(plyr)
 library(dplyr)
 library(tidytext)
 library(ROCR)
-######################################################################################
 
 ######################################################################################
-# load the data
-load("../data/sales_scraped_information.RData")
-######################################################################################
+#
 
-######################################################################################
-
-# first, clean the data like dollars and data
+#first, clean the data like dollars and data
 sale_bitcoin_dollar <- c()
 sale_history_sub <- c()
 for(i in 1:length(sale_history)){
   sale_history_sub[[i]] <- as.data.frame(sale_history[[i]])
-  sale_history_sub[[i]] <- sale_history_sub[[i]] %>% 
-    mutate(dollar = str_split_fixed(sale_history_sub[[i]]$V4, "??", 2))
-  
-  sale_history_sub[[i]]$dollar[,2]<- gsub('[M]',"000000", sale_history_sub[[i]]$dollar[,2])# covert "M" to "00000"
-  sale_history_sub[[i]]$dollar[,2]<- gsub('[B]',"00000000", sale_history_sub[[i]]$dollar[,2])# covert "B" to "000000000"
-  sale_history_sub[[i]]$dollar[,2]<- as.numeric(gsub('[$,().<Y]',"", sale_history_sub[[i]]$dollar[,2]))# change all number with symbol to numerics form
-  #View all punk's "sold","bid","offered" which smaller than 0.01 as 1 dollar
-  #sale_history[[i]]$dollar[,2]<- as.numeric(gsub('M',"00000", sale_history[[i]]$dollar[,2]))
-  
+  sale_history_sub[[i]] <- sale_history_sub[[i]] %>% mutate(
+    dollar = str_split_fixed(sale_history_sub[[i]]$V4, "??", 2))
+  sale_history_sub[[i]]$dollar[,2]<- gsub('[$,(),<]',"", sale_history_sub[[i]]$dollar[,2])# change all number with symbol to numerics form
+   sale_history_sub[[i]]$dollar[,2]<- gsub("M" ,"e6", sale_history_sub[[i]]$dollar[,2])
+   sale_history_sub[[i]]$dollar[,2]<- gsub("B" ,"e8", sale_history_sub[[i]]$dollar[,2])
+   
+    
+    sale_history_sub[[i]]$dollar[,2]<- as.numeric(sale_history_sub[[i]]$dollar[,2])
+    #View all punk's "sold","bid","offered" which smaller than 0.01 as 1 dollar
 }
+
 
 #clean the date
 for(i in 1:length(sale_history)){
@@ -82,7 +78,20 @@ for(i in 1:length(sale_history_sub)){
 #bind all transactions to one data_frame
 result <- do.call(rbind,test)
 #connect each sell to their own punk
-result <- result %>% select(-index2)%>% 
+result <- result %>% select(-index2,-largest_offer)%>% 
   relocate(punk, .before = bid)
+
+#add last sold_price corresponde to the sold price
+last_sold_price <- result$sold_price[result$sold_price !=177275]
+Final_result <- cbind_fill(result,last_sold_price)
 write.csv(result, "result.csv")
-######################################################################################
+
+#find the position of first  transaction and delete it
+for(i in 1:length(Final_result$punk)){
+  Final_result$position_last_sold[[i]] <- ifelse(Final_result$punk[[i]] + 1 != Final_result$punk[[i+1]],0,1)
+}
+Final_result$X2[Final_result$position_last_sold == "1"] <- "NA" 
+#drop the index colmn and rename the X2
+Final_result <- Final_result %>% select(-position_last_sold)%>%mutate(
+  last_sold_price = X2)%>%select(-X2)
+write.csv(Final_result, "Final_result.csv")
--- MDML_final_project/scripts/main.R
+++ MDML_final_project/scripts/main.R
@@ -14,14 +14,14 @@ library(rvest)
 load("../data/punk_gender.RData")
 load("../data/punk_image_RGBA.RData")
 punk_attributes <- read_csv("../data/punk_attributes.csv")
-sale_history <- read_csv("../data/sale_history_account.csv")
+sale_history <- read_csv("../data/Sale_History_Data_Cleaning_Final_result.csv")
 ######################################################################################
 
 ######################################################################################
 # unify the data
 punk_attributes <- punk_attributes %>% 
   mutate(punk_name = punk_gender$punk_name[id]) %>% 
-  select(-id,-attributes) %>% 
+  select(-attributes) %>% 
   relocate(punk_name)
 
 sale_history <- sale_history %>% 
@@ -44,7 +44,32 @@ sale_data <- sale_data %>%
 
 
 
+######################################################################################
+
+######################################################################################
+train
+
+validate
+
+auc
 
 
+# predicting outcome as a function of cuisine, borough, month, weekday, and the four historical features 
+formula <- "outcome ~ cuisine + borough + month + weekday + 
+                      num_previous_low_inspections + num_previous_med_inspections + 
+                      num_previous_high_inspections + num_previous_closings"
 
+# Fit a random forest model on train
+# Use 1000 trees, and make sure that both respect.unordered.factors and probability are TRUE
+# other settings default values
+fit.rf <- ranger(formula, data = train, num.trees = 1000,
+                 respect.unordered.factors = T, probability = T)
+
+# compute the AUC of this model on test.
+test <- as_tibble(test) %>% 
+  mutate(predicted.probability.rf = predict(fit.rf, test, type='response')$predictions[,2])
+
+test.pred <- prediction(test$predicted.probability.rf, test$outcome)
+test.perf <- performance(test.pred, "auc")
+cat('the auc score is ', test.perf@y.values[[1]], "\n")
 ######################################################################################
